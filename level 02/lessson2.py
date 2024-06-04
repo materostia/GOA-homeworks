fea860f548004188d5c4f10d4cb5ef841ea0728d
materostia <materostia@gmail.com>
2024-06-04 17:38:49 +0400

    second commit

--- level 02/lessson2.py
+++ level 02/lessson2.py
@@ -1,1 +1,3 @@
-print("davit")
+salary = 900
+new_salary = salary + 200
+print(new_salary)
---
+++ level06/classwork/classwork.py
@@ -0,0 +1,11 @@
+name = "data"
+surname = "diasamidze"
+age = 100
+text = "I am programmer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+salary = 1000
+pay_raise = 100
+new_salary = salary + pay_raise
+print(salary)
